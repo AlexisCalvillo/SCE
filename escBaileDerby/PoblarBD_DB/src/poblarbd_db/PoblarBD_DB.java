16713e7aa490a9e38cfad1fc7dd96d6dca428b79
Alexis Calvillo <acalvillm@gmail.com>
2020-09-19 00:34:06 -0500

    Generación de scripts para poblar bd Derby en clase PoblarBD_DB

---
+++ escBaileDerby/PoblarBD_DB/src/poblarbd_db/PoblarBD_DB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poblarbd_db;
+
+import java.sql.ResultSet;
+
+
+/**
+ *
+ * @author aalex
+ */
+public class PoblarBD_DB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ResultSet r = null;
+        java.util.TreeMap<String,ClsCampoBD> colCampos = null;
+        String[] strTb;
+        if(args.length!=0)
+            strTb=args;
+        else
+            strTb=new String[]{"tblAlumnos","tblAlumnosXGrupo","tblBailes","tblGrupos","tblNiveles","tblPagosaProfesores","tblProfesores","tblUsuarios"};
+        String strLocBD = "C:\\Users\\aalex\\Documents\\NetBeansProjects\\SCE\\escBaileDerby\\PoblarBD_DB\\EscDeBaile";
+        try
+        {
+           ClsConexion c = new ClsConexion(strLocBD);
+           c.conectate("rafa","rafa");
+           if( c.conectado() )    
+           {
+               for(int i=0;i<strTb.length;i++){
+                   r = c.obtenRS(strTb[i]);
+                   colCampos = c.obtenMapaCampos(r);
+                   r = c.obtenRS(strTb[i],colCampos );
+                   System.out.println(c.obtenCadenaInsertRs(strTb[i], r));
+               }
+           }
+           c.cierraCon();
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+}
