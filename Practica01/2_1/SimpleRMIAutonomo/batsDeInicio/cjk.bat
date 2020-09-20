7dda1072314144de7af1e42321459866a6e7f16f
Alexis Calvillo <acalvillm@gmail.com>
2020-09-19 23:33:11 -0500

    Cambios a los bats

--- Practica01/2_1/SimpleRMIAutonomo/batsDeInicio/cjk.bat
+++ Practica01/2_1/SimpleRMIAutonomo/batsDeInicio/cjk.bat
@@ -1,5 +1,5 @@
 set path=C:\Program Files\Java\jre1.8.0_261\bin;%path%
 set classpath=.;C:\Program Files\Java\jre1.8.0_261;%classpath%
-cd C:\Users\aalex\OneDrive\ITAM\ultMat\SCE\SimpleRMIAutonomo
+cd C:\Users\aalex\Documents\NetBeansProjects\SCE\Practica01\2_1\SimpleRMIAutonomo\
 c:
 
--- Practica01/2_3/RMITarea01/batsDeInicio/cjk.bat
+++ Practica01/2_3/RMITarea01/batsDeInicio/cjk.bat
@@ -1,5 +1,5 @@
 set path=C:\Program Files\Java\jre1.8.0_261\bin;%path%
 set classpath=.;C:\Program Files\Java\jre1.8.0_261;%classpath%
-cd C:\Users\aalex\Documents\NetBeansProjects\Tarea01\RMITarea01
+cd C:\Users\aalex\Documents\NetBeansProjects\SCE\Practica01\2_3\RMITarea01\
 c:
 
